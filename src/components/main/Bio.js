153e2ec19ec8d40abd7bc9d0b31de8a1b33763a5
Andrew Luther <andrewgluther@gmail.com>
2025-01-12 14:02:12 -0500

    Updated bio

--- src/components/main/Bio.js
+++ src/components/main/Bio.js
@@ -10,7 +10,12 @@ function Bio() {
         interested in the application of computer science for electronic music
         composition, production, and analysis.
         <br />
-        <br />I graduated with a Bachelor of Music (Honours) in 2021 from
+        <br />In 2024, I graduated with a Bachelor of Science in
+        Computer Science at Memorial University, and I have been developing my
+        programming and research skills. You can find some of my programming
+        projects <Link to="/projects">here</Link>.
+        <br />
+        <br />Additionally, I graduated with a Bachelor of Music (Honours) in 2021 from
         Memorial University, where I studied piano under Dr. Kristina Szutor and
         music composition under Dr. Andrew Staniland and Dr. Clark Ross. You can
         find some of my music compositions <Link to="/compositions">here</Link>.
@@ -18,16 +23,12 @@ function Bio() {
         under the direction of professors Florian Hoefner and Bill Brennan,
         respectively.
         <br />
-        <br />I will soon complete a Bachelor of Science with a major in
-        Computer Science at Memorial University, and I have been developing my
-        programming and research skills. You can find some of my programming
-        projects <Link to="/projects">here</Link>. Recently, I completed a
-        12-month internship at Compusult Ltd. in Mount Pearl, NL, where I worked
-        as a Programmer/Developer.
-        <br />
-        <br />
+        <br />Currently, I am working on a Master's of Computer Science at York University, under the supervision of
+        Dr. Vassilios Tzerpos. My thesis will focus on using AI to create useful tools for composers and musicians.
+        <br/>
+        <br/>
         When I am not coding or composing, I enjoy hiking, gaming, and listening
-        to music (of course!).
+        to music.
       </p>
     </div>
   );
